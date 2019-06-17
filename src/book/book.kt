580bf73f653b168024a6492b71c86183bff69fc1
Haider Ali <haider.lee23@gmail.com>
2019-06-17 22:16:40 +0530

    Pairs in Kotlin

--- src/book/book.kt
+++ src/book/book.kt
@@ -1,15 +1,41 @@
 package book
 
-open class Book(val title: String, val author: String) {
-    private var currentPage = 0
-    open fun readPage() {
-        currentPage++
+import java.time.Year
+
+//open class Book(val title: String, val author: String) {
+//    private var currentPage = 0
+//    open fun readPage() {
+//        currentPage++
+//    }
+//}
+//
+//class eBook(title: String, author: String, val format: String = "text"): Book(title, author) {
+//    private var wordCount = 0
+//    override fun readPage() {
+//        wordCount += 250
+//    }
+//}
+
+class Book {
+    fun titleAuthor(title: String, author: String): Pair<String, String> {
+        return (title to author)
+    }
+
+    fun titleAuthorYear(title: String, author: String, year: String): Triple<String, String, String> {
+        return Triple(title, author, year)
     }
 }
 
-class eBook(title: String, author: String, val format: String = "text"): Book(title, author) {
-    private var wordCount = 0
-    override fun readPage() {
-        wordCount += 250
-    }
+fun main() {
+    val myBook = Book()
+
+    println(myBook.titleAuthor(title = "title", author = "author"))
+
+    val book = myBook.titleAuthorYear(title = "title", author = "author", year = "year")
+
+    println(myBook.titleAuthorYear(title = "title", author = "author", year = "year"))
+
+    println("here is your book ${book.first} " +
+            "written by ${book.second} " +
+            "in the year ${book.third}")
 }
